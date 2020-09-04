b2d5cf6389985f284badde78621a24136e2c081f
moloko666 <moloko666777@gmail.com>
2020-09-04 16:23:21 +0300

    add swiper slider

---
+++ assets/js/configs/slider.js
@@ -0,0 +1,35 @@
+new Swiper('.swiper-container',{
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    speed: 400,
+    pagination: {
+        el: '.swiper-pagination-1',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    // breakpoints: {
+    //     320: {
+    //         spaceBetween: 40,
+    //         slidesPerView: 1,
+    //         slidesPerGroup: 1,
+    //     },
+    //     430: {
+    //         spaceBetween: 40,
+    //         slidesPerView: 2,
+    //         slidesPerGroup: 2,
+    //     },
+    //     768: {
+    //         spaceBetween: 45,
+    //         slidesPerView: 3,
+    //         slidesPerGroup: 3,
+    //     },
+    //     1024: {
+    //         spaceBetween: 80,
+    //         slidesPerView: 3,
+    //         slidesPerGroup: 3,
+    //     }
+    // }
+})
