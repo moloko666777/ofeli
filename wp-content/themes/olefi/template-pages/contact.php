4d5e497ad16886a3a0daffb8467f250944180f74
minkoj2 <web.bagach@gmail.com>
2021-08-30 22:24:52 +0300

    add plugins & add templates for pages

---
+++ wp-content/themes/olefi/template-pages/contact.php
@@ -0,0 +1,37 @@
+<?php
+/* Template name: Шаблон "Контакты" */
+get_header(); ?>
+
+<main>
+    <section class="">
+        <div class="contact">
+            <div class="contact__wrapper container-xxl">
+                <div class="contact__border wow animate__animated animate__fadeInRight">
+                    <div class="contact__border-top-left"></div>
+                    <div class="contact__border-bottom-right"></div>
+                </div>
+                <div class="contact__title wow animate__animated animate__fadeIn">
+                    <h2>НАШИ КОНТАКТЫ</h2>
+                    <div class="contact__row row">
+                        <div class="col-xl-6 col-md-12">
+                            <div class="contact__address">
+                                <p>Адрес: <span>г. Харьков, Пушкина,100</span></p>
+                                <p>Телефон: <a href="tel:+38 (066) 1234567">+38 (066) 1234567</a></p>
+                                <p>Instagram: <a href="https://www.instagram.com/ofeli_salon"> Ofeli_salon </a></p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-12">
+                            <div class="contact__time">
+                                <p><span>Часы работы:</span><br>
+                                    ПН-ВС 10:00-20:00 <br>
+                                    без перерывов и выходных</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
