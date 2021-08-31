a34693fffc4e8ca701de5d946ecfdebbbbcfbdc0
moloko666777 <moloko666777@gmail.com>
2021-08-31 20:18:55 +0300

    add new menu

--- assets/js/configs/home-page.js
+++ assets/js/configs/home-page.js
@@ -1,16 +1,47 @@
-let openMenu = document.querySelector('.header__menu');
-let menu = document.querySelector('.header__menu-links');
-let closeMenu = document.querySelector('.header__menu-close');
-let closeMenuLeft = document.querySelector('.header__menu-left');
-let removeScroll = document.querySelector('body');
+// let openMenu = document.querySelector('.header__menu');
+// let menu = document.querySelector('.header__menu-links');
+// let closeMenu = document.querySelector('.header__menu-close');
+// let closeMenuLeft = document.querySelector('.header__menu-left');
+// let removeScroll = document.querySelector('body');
+//
+// openMenu.addEventListener('click', function (){
+//     menu.classList.add('show');
+//     removeScroll.style.overflow = 'hidden';
+//     removeScroll.style.paddingRight = '5px';
+// })
+//
+// closeMenu.addEventListener('click', function (){
+//     menu.classList.remove('show');
+//     removeScroll.removeAttribute('style');
+// })
 
-openMenu.addEventListener('click', function (){
-    menu.classList.add('show');
-    removeScroll.style.overflow = 'hidden';
-    removeScroll.style.paddingRight = '5px';
-})
 
-closeMenu.addEventListener('click', function (){
-    menu.classList.remove('show');
-    removeScroll.removeAttribute('style');
-})
+
+
+
+        //Navigation
+
+        var app = function () {
+            var body = undefined;
+            var menu = undefined;
+            var menuItems = undefined;
+            var init = function init() {
+                body = document.querySelector('body');
+                menu = document.querySelector('.menu-icon');
+                menuItems = document.querySelectorAll('.nav__list-item');
+                applyListeners();
+            };
+            var applyListeners = function applyListeners() {
+                menu.addEventListener('click', function () {
+                    return toggleClass(body, 'nav-active');
+                });
+            };
+            var toggleClass = function toggleClass(element, stringClass) {
+                if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+            };
+            init();
+        }();
+
+
+
+
